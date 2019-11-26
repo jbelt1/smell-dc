835cf68753ffc443631dff6339175ca24adcafcb
Kevin Aranyi <kevinaranyi@wireless-10-104-126-247.umd.edu>
2019-11-26 14:52:58 -0500

    location client

--- SmellDC/app/src/main/java/com/example/smelldc/DashboardActivity.kt
+++ SmellDC/app/src/main/java/com/example/smelldc/DashboardActivity.kt
@@ -1,24 +1,28 @@
 package com.example.smelldc
 
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.SeekBar
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.security.Permission
 
 class DashboardActivity : AppCompatActivity() {
 
-    private var mAirQuality: SeekBar? = null
-    private var mSymptoms: EditText? = null
-    private var mActivity: EditText? = null
-    private var mSendToHealthDpt: CheckBox? = null
-    private var mSubmitBtn: Button? = null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +37,34 @@ class DashboardActivity : AppCompatActivity() {
             R.id.navigation_report, R.id.navigation_map, R.id.navigation_profile))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("SMELL-DC", "DENIED")
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        } else {
+            Log.i("SMELL-DC", "GRANTED")
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i("SMELL-DC", "PERMISSION REQUEST GRANTED")
+            } else {
+                Log.i("SMELL-DC", "error")
+            }
+        }
+    }
+
+
+    fun getCurrentLocation() : Task<Location>? {
+        return fusedLocationClient.lastLocation
     }
 
 }
--- SmellDC/app/src/main/java/com/example/smelldc/ui/report/ReportFragment.kt
+++ SmellDC/app/src/main/java/com/example/smelldc/ui/report/ReportFragment.kt
@@ -1,16 +1,31 @@
 package com.example.smelldc.ui.report
 
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.example.smelldc.DashboardActivity
 import com.example.smelldc.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class ReportFragment : Fragment() {
 
     private lateinit var dashboardViewModel: ReportViewModel
+
+    private var mAirQuality: SeekBar? = null
+    private var mSymptoms: EditText? = null
+    private var mActivity: EditText? = null
+    private var mSendToHealthDpt: CheckBox? = null
+    private var mSubmitBtn: Button? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +35,19 @@ class ReportFragment : Fragment() {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(ReportViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_report, container, false)
+
+        mSubmitBtn = root.findViewById<Button>(R.id.submitBtn) as Button
+        mSubmitBtn?.setOnClickListener {
+            val activity = activity as DashboardActivity
+            activity.getCurrentLocation()?.addOnSuccessListener { location : Location? ->
+                // Got last known location. In some rare situations this can be null.
+
+                // save report data to db and update global data structure
+                Log.i("SMELL-DC", "Latitude: " + location?.latitude)
+            }
+        }
+
         return root
     }
+
 }
