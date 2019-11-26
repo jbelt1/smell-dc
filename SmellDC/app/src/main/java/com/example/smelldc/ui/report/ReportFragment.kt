af2f18861a93f8d82e6f3dca2288517b98c2cf3a
Kevin Aranyi <kevinaranyi@wireless-10-104-67-146.umd.edu>
2019-11-26 16:17:18 -0500

    add reports

--- SmellDC/app/src/main/java/com/example/smelldc/ui/report/ReportFragment.kt
+++ SmellDC/app/src/main/java/com/example/smelldc/ui/report/ReportFragment.kt
@@ -14,13 +14,17 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.smelldc.DashboardActivity
 import com.example.smelldc.R
+import com.example.smelldc.ui.map.Report
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
 
 class ReportFragment : Fragment() {
 
     private lateinit var dashboardViewModel: ReportViewModel
 
+    private val db = FirebaseFirestore.getInstance()
     private var mAirQuality: SeekBar? = null
     private var mSymptoms: EditText? = null
     private var mActivity: EditText? = null
@@ -37,10 +41,29 @@ class ReportFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_report, container, false)
 
         mSubmitBtn = root.findViewById<Button>(R.id.submitBtn) as Button
+        mAirQuality = root.findViewById<SeekBar>(R.id.seekBar) as SeekBar
+        mSymptoms = root.findViewById<EditText>(R.id.symptoms) as EditText
+        mActivity = root.findViewById<EditText>(R.id.activity) as EditText
+        mSendToHealthDpt = root.findViewById<CheckBox>(R.id.checkBox) as CheckBox
+
         mSubmitBtn?.setOnClickListener {
             val activity = activity as DashboardActivity
             activity.getCurrentLocation()?.addOnSuccessListener { location : Location? ->
                 // Got last known location. In some rare situations this can be null.
+                val geoPoint = GeoPoint(location!!.latitude, location!!.longitude)
+
+                val report = Report(mActivity?.text.toString(), geoPoint, "smoke", mAirQuality?.progress,
+                    mSymptoms?.text.toString(), mSendToHealthDpt?.isChecked)
+
+                db.collection("reports")
+                    .add(report)
+                    .addOnSuccessListener { documentReference ->
+                        Log.i("SMELL-DC", "DocumentSnapshot added with ID: ${documentReference.id}")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.i("SMELL-DC", "Error adding document", e)
+                    }
+
 
                 // save report data to db and update global data structure
                 Log.i("SMELL-DC", "Latitude: " + location?.latitude)
